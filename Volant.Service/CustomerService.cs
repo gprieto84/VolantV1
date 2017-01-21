9f27db8acec9bb7b288fdbd2bf52ba284c0c2947
gprieto84 <german.prt@gmail.com>
2017-01-21 09:37:07 -0500

    Version 0.0.0.3

---
+++ Volant.Service/CustomerService.cs
@@ -0,0 +1,66 @@
+﻿using Volant.Data.Infrastructure;
+using Volant.Data.Repositories;
+using Volant.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volant.Service
+{
+    public interface ICustomerService
+    {
+        IEnumerable<Customer> GetCustomers(string name = null);
+        Customer GetCustomer(int id);
+        Customer GetCustomer(string name);
+        void CreateCustomer(Customer customer);
+        void SaveCustomer();
+    }
+
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerRepository customerRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public CustomerService(ICustomerRepository customerRepository, IUnitOfWork unitOfWork)
+        {
+            this.customerRepository = customerRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        #region ICustomerService Members
+
+        public IEnumerable<Customer> GetCustomers(string name = null)
+        {
+            if (string.IsNullOrEmpty(name))
+                return customerRepository.GetAll();
+            else
+                return customerRepository.GetAll().Where(c => c.name == name);
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            var customer = customerRepository.GetById(id);
+            return customer;
+        }
+
+        public Customer GetCustomer(string name)
+        {
+            var customer = customerRepository.GetCustomerByName(name);
+            return customer;
+        }
+
+        public void CreateCustomer(Customer customer)
+        {
+            customerRepository.Add(customer);
+        }
+
+        public void SaveCustomer()
+        {
+            unitOfWork.Commit();
+        }
+
+        #endregion
+    }
+}
---
+++ Volant.Service/ProjectService.cs
@@ -0,0 +1,68 @@
+﻿using Volant.Data.Infrastructure;
+using Volant.Data.Repositories;
+using Volant.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Volant.Service
+{
+    public interface IProjectService
+    {
+        IEnumerable<Project> GetProjects();
+        IEnumerable<Project> GetCustomerProjects(string customerName, string projectName = null);
+        Project GetProject(int id);
+        void CreateProject(Project project);
+        void SaveProject();
+    }
+
+    public class ProjectService : IProjectService
+    {
+        private readonly IProjectRepository projectRepository;
+        private readonly ICustomerRepository customerRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public ProjectService(IProjectRepository projectRepository, ICustomerRepository customerRepository, IUnitOfWork unitOfWork)
+        {
+            this.projectRepository = projectRepository;
+            this.customerRepository = customerRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        #region IProjectService Members
+
+        public IEnumerable<Project> GetProjects()
+        {
+            var project = projectRepository.GetAll();
+            return project;
+        }
+
+        public IEnumerable<Project> GetCustomerProjects(string customerName, string projectName = null)
+        {
+            var customer = customerRepository.GetCustomerByName(customerName);
+            return customer.projects.Where(g => g.name.ToLower().Contains(customerName.ToLower().Trim()));
+        }
+
+        public Project GetProject(int id)
+        {
+            var project = projectRepository.GetById(id);
+            return project;
+        }
+
+        public void CreateProject(Project project)
+        {
+            projectRepository.Add(project);
+        }
+
+        public void SaveProject()
+        {
+            unitOfWork.Commit();
+        }
+
+        #endregion
+
+    }
+}
